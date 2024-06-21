5f42d92d742418667e1a703386b7d88a87d3a046
kyaukyuai <y.kakui@gmail.com>
2024-06-21 15:24:59 +0900

    refactor(constants): Update site info and feed list

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,20 +1,20 @@
-const siteUrlStem = 'https://yamadashy.github.io/tech-blog-rss-feed';
+const siteUrlStem = 'https://kyaukyuai.github.io/llm-tech-blog-rss-feed';
 const siteUrl = `${siteUrlStem}/`;
 
 module.exports = {
   // サイト設定
   siteUrl: `${siteUrl}`,
   siteUrlStem: siteUrlStem,
-  siteTitle: '企業テックブログRSS',
+  siteTitle: 'LLMテックブログRSS',
   siteDescription:
-    '企業のテックブログの更新をまとめたRSSフィードを配信しています。記事を読んでその企業の技術・カルチャーを知れることや、質の高い技術情報を得られることを目的としています。',
+    'LLMのテックブログの更新をまとめたRSSフィードを配信しています。記事を読んでその企業の技術・カルチャーを知れることや、質の高い技術情報を得られることを目的としています。',
 
   // フィード設定
-  feedTitle: '企業テックブログRSS',
-  feedDescription: '企業のテックブログの更新をまとめたRSSフィード',
+  feedTitle: 'LLMテックブログRSS',
+  feedDescription: 'LLMのテックブログの更新をまとめたRSSフィード',
   feedLanguage: 'ja',
-  feedCopyright: 'yamadashy/tech-blog-rss-feed',
-  feedGenerator: 'yamadashy/tech-blog-rss-feed',
+  feedCopyright: 'kyaukyuai/llm-tech-blog-rss-feed',
+  feedGenerator: 'kyaukyuai/llm-tech-blog-rss-feed',
   feedUrls: {
     atom: `${siteUrl}feeds/atom.xml`,
     rss: `${siteUrl}feeds/rss.xml`,
@@ -22,18 +22,18 @@ module.exports = {
   },
 
   // GitHub
-  author: 'yamadashy',
-  gitHubUserUrl: 'https://github.com/yamadashy/',
-  gitHubRepositoryUrl: 'https://github.com/yamadashy/tech-blog-rss-feed/',
+  author: 'kyaukyuai',
+  gitHubUserUrl: 'https://github.com/kyaukyuai/',
+  gitHubRepositoryUrl: 'https://github.com/kyaukyuai/llm-tech-blog-rss-feed/',
 
   // Google Analytics系。フォークして使う際は値を空にするか書き換えてください
-  googleSiteVerification: 'GPLvXv8kYtLMW912ZS54DKFEZL6ruOrjOFLdHVTo37o',
-  globalSiteTagKey: 'G-CNNNTL0NB3',
+  googleSiteVerification: '',
+  globalSiteTagKey: '',
 
   // フィードの取得などに使う UserAgent
-  requestUserAgent: 'facebookexternalhit/1.1; yamadashy/tech-blog-rss-feed',
+  requestUserAgent: 'facebookexternalhit/1.1; kyaukyuai/llm-tech-blog-rss-feed',
 
   // サイトの追加方法のリンク
   howToAddSiteLink:
-    'https://github.com/yamadashy/tech-blog-rss-feed#%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AE%E8%BF%BD%E5%8A%A0%E6%96%B9%E6%B3%95',
+    'https://github.com/kyaukyuai/llm-tech-blog-rss-feed#%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AE%E8%BF%BD%E5%8A%A0%E6%96%B9%E6%B3%95',
 };
